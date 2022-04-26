960e64d82670075f1ea18f31c702d150c14bfc42
xaim-dev <yurabendzo20@gmail.com>
2022-04-26 12:06:46 +0300

    complete task 3

---
+++ task_3/weather-and-other-task/src/components/PersimmonComp/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="header">
+        <div class="logo__inner">
+          <img class="logo__img" :src="logo" />
+          <div class="logo__text">{{ logoText }}</div>
+        </div>
+        <div class="header__text">{{ headerText }}</div>
+      </div>
+      <div class="content">
+        <img class="content__img" :src="contentImage" />
+        <div class="content__title">{{ contentTitle }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PersimmonComp",
+
+  props: {
+    logo: {
+      type: String,
+      default: "http://giuseppe.com.ua/img/no-photo.png",
+    },
+    logoText: {
+      type: String,
+    },
+    headerText: {
+      type: String,
+    },
+    contentImage: {
+      type: String,
+      default: "http://giuseppe.com.ua/img/no-photo.png",
+    },
+    contentTitle: {
+      type: String,
+      default: "no title",
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.wrapper {
+  max-width: 400px;
+  margin-bottom: 30px;
+}
+.header {
+  background-color: #181515;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.logo__inner {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+.logo__img {
+  max-width: 30px;
+}
+.logo__text {
+  color: #ff8016;
+  font-family: "Rubik Moonrocks", cursive;
+  font-weight: 400;
+  font-size: 16px;
+  padding-left: 5px;
+}
+.header__text {
+  color: #fff;
+  font-family: "Grape Nuts", cursive;
+  font-weight: 400;
+  font-size: 18px;
+  padding-right: 10px;
+}
+.content {
+  background-color: #d5cccc;
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+.content__img {
+  max-width: 250px;
+  padding: 10px 0;
+  border-radius: 60px;
+}
+.content__title {
+  color: #ff8016;
+  font-family: "Grape Nuts", cursive;
+  font-weight: 400;
+  font-size: 22px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  text-align: left;
+}
+</style>
---
+++ task_3/weather-and-other-task/src/components/WeatherComp/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <form class="form">
+      <div class="weather__top">
+        <select class="select__city" v-model="weatherList">
+          <option v-for="weather in weatherListData" :key="weather.id"
+                  :value="weather.city"> {{weather.city}}</option>
+        </select>
+        <div class="weather__top__time">{{ todayTime }}</div>
+      </div>
+      <div class="container">
+        <div
+        class="weather__in__city"
+        v-for="weather in filteredWeatherListData"
+        :key="weather.id"
+      >
+        <div class="weather__temp">
+          <img class="term__img" :src="weather.termImg" />
+          <div class="temprature">{{ weather.temp }}</div>
+        </div>
+        <div class="weather__param">
+          <div class="humidity">
+            <span>Вологість:</span> {{ weather.humid }}
+          </div>
+          <div class="weather__pressure">
+            <span>Тиск:</span> {{ weather.pressure }}
+          </div>
+          <div class="weather__wind">
+            <span>Вітер:</span> {{ weather.wind }}
+          </div>
+        </div>
+      </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherComp",
+  props: {
+    weatherListData: {
+      type: Array,
+      default: () => [],
+    },
+    todayTime: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      weatherList: null,
+    };
+  },
+  computed: {
+    filteredWeatherListData() {
+      return  this.weatherListData.filter((weather) =>
+          weather.city.includes(this.weatherList
+        ));
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.form {
+  margin-bottom: 20px;
+}
+.weather__top {
+  display: flex;
+  margin-bottom: 15px;
+}
+.weather__top__time {
+  margin-left: 10px;
+}
+.weather__in__city {
+  display: flex;
+  align-items: center;
+}
+.weather__temp {
+  display: flex;
+  align-items: center;
+}
+.temprature {
+  font-size: 22px;
+}
+.weather__param {
+  margin-left: 60px;
+  font-size: 16px;
+}
+.weather__param span {
+  color: gray;
+  font-size: 14px;
+}
+.term__img {
+  max-width: 50px;
+}
+</style>
